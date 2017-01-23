87fe824942b09d3b01b4fdb73f9741efd91afb40
Alvin BERTHELOT <alvin.berthelot@webyousoon.com>
2017-01-23 17:10:50 +0100

    Solution exercise 3

--- src/product.js
+++ src/product.js
@@ -1,38 +1,38 @@
 var chalk = require('chalk');
 
-function Product(name, price) {
-  this.name = name;
-  this.price = price;
-  this.toString = function(currency, rateCurrency) {
+
+class Product {
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
+  }
+  toString(currency, rateCurrency) {
     var currency = currency || '$';
     var rateCurrency = rateCurrency || 1;
-    return chalk.yellow(this.name) + ' cost ' + chalk.red(this.price * rateCurrency) + currency;
+    return `${chalk.yellow(this.name)} cost ${chalk.red(this.price * rateCurrency)}${currency}`;
   }
 }
 
-function Book(name, price, isbn) {
-  Product.apply(this, [name, price]);
-  this.isbn = isbn;
+class Book extends Product {
+  constructor(name, price, isbn) {
+    super(name, price);
+    this.isbn = isbn;
+  }
 }
-Book.prototype = Object.create(Product.prototype, {
-  constructor: {value: Book}
-});
 
-function DVD(name, price, moovie) {
-  Product.apply(this, [name, price]);
-  this.moovie = moovie;
+class DVD extends Product {
+  constructor(name, price, moovie) {
+    super(name, price);
+    this.moovie = moovie;
+  }
 }
-DVD.prototype = Object.create(Product.prototype, {
-  constructor: {value: DVD}
-});
 
-function VideoGame(name, price, platform) {
-  Product.apply(this, [name, price]);
-  this.platform = platform;
+class VideoGame extends Product {
+  constructor(name, price, platform) {
+    super(name, price);
+    this.platform = platform;
+  }
 }
-DVD.VideoGame = Object.create(Product.prototype, {
-  constructor: {value: VideoGame}
-});
 
 
 module.exports = {
